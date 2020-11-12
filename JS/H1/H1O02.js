0e61a425c52d6f40bb34d6660785844f9eef4a7b
TimKooistra <154630@csgaugustinus.nl>
2020-11-12 09:08:07 +0000

    x

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -9,11 +9,17 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+  noStroke();
+  fill('darkred');
+  triangle(0,0,225,450,450,0);
 
   noStroke();
   fill('pink');
   triangle(450,0,0,225,450,450)
   noStroke();
   fill('purple');
-  triangle(0,225,0,450,0,0)
+  triangle(450,225,0,450,0,0)
+
+ 
+
 }
--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -3,16 +3,19 @@ function setup() {
   background('white');
   canvas.parent('processing');
   noLoop();
-  // colorMode(RGB,255,255,255,1);  
+  colorMode(RGB,255,255,255,1);  
 }
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
-  fill('red');
+  fill(0,0,255,5.3);
+  fill(0,0,225,0.3);
   rect(0,150,450,150);
-  fill('green');
+  fill(0,0,225,0.4);
   rect(0,0,150,450);
-  fill('blue');  
+  fill(0,0,225,0.4);  
   rect(75,0,375,450);
+  ellipse(450,225,450);
+  fill(0,0,225,0.3)
+  triangle(0,0,225,450,450,225)
 }
